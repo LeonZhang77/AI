37f2190904f314b9c5ac72a850d3198f58cc4fd5
Leon Zhang <zhangjianfeng73@hotmail./com>
2026-06-04 10:18:12 +0800

    save and load onxx

--- MLP-TensorFlow/main.py
+++ MLP-TensorFlow/main.py
@@ -13,24 +13,8 @@ import onnx
 # 导入onnx Runtime 库 用于加载 onnx模型 进行推理验证
 import onnxruntime as ort
 
-# 按装订区域中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print("Hello World")
-    # 1. 输入三个条件，结果：是否旅游？
-    # x1 = 1.0(天气好), x2 = 1.0(有空), x3 = 0.0(身体累)
-    X_train = np.array([
-        [1, 1, 0],
-        [1, 1, 1],
-        [1, 0, 0],
-        [1, 0, 1],
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 0, 0],
-        [0, 0, 1]
-    ], dtype=np.float32)
-    # 真实标签 1.0 表示去旅游
-    y_train = np.array([[1],[0], [0], [0], [0], [0], [0],[0]], dtype=np.float32)
 
+def set_model(X_train, y_train):
     # 2 定义MLP模型
     # 优化：神经元个数=特征数量 3 + 输出 1 + 冗余 = 4 ~ 6 个
     # model = Sequential([
@@ -57,7 +41,9 @@ if __name__ == '__main__':
     model.compile(optimizer=Adam(0.01),
                   loss='binary_crossentropy',
                   metrics=['accuracy'])  # 跟踪损失值
+    return model
 
+def train_model(model, X_train, y_train):
     # 4 训练模型
     model.fit(X_train, y_train, epochs=5000, verbose=1, validation_split=0.2)
 
@@ -77,15 +63,56 @@ if __name__ == '__main__':
     X_new2 = np.array([[0.0, 1.0, 1.0]])
     pred2 = model.predict(X_new2)
     print(f"tensorflow [0, 1, 1]预测概率:{pred2[0][0]:.6f}")
+    return
 
+def save_onnx_model(model, onnx_model_path):
     # ---------------------保存为ONNX----------------------
     # opset=13 指定ONNX算子集版本 确认兼容性
     # 1、将tensorflow 模型转换为onnx 模型
     onnx_model, _ = tf2onnx.convert.from_keras(model, opset=13)
 
     # 2、保存onnx_model
-    onnx_model_path = "train_model.onnx"
     onnx.save(onnx_model, onnx_model_path)
     print(f"onnx 模型 已保存为:{onnx_model_path}")
+    return
+
+# 按装订区域中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    print("Hello World")
+    # 1. 输入三个条件，结果：是否旅游？
+    # x1 = 1.0(天气好), x2 = 1.0(有空), x3 = 0.0(身体累)
+    X_train = np.array([
+        [1, 1, 0],
+        [1, 1, 1],
+        [1, 0, 0],
+        [1, 0, 1],
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 0, 0],
+        [0, 0, 1]
+    ], dtype=np.float32)
+    # 真实标签 1.0 表示去旅游
+    y_train = np.array([[1], [0], [0], [0], [0], [0], [0], [0]], dtype=np.float32)
+
+    # model = set_model(X_train, y_train)
+    #
+    # train_model(model, X_train, y_train)
+    #
+    # save_onnx_model(model, "train_model.onnx")
+
+    # ---------------------使用ONNX模型预测----------------------
+    # 1、加载
+    ort_session = ort.InferenceSession("train_model.onnx")
+    # 2、获取 输入层和输出层
+    input_name = ort_session.get_inputs()[0].name
+    output_name = ort_session.get_outputs()[0].name
+    # 3、预测
+    X_new3 = np.array([[1.0, 1.0, 0.0]], dtype=np.float32)
+    onnx_result = ort_session.run([output_name], {input_name: X_new3})[0]
+    print(f"onnx模型预测概率[1.0, 1.0, 0]:{onnx_result[0][0]:.6f}")
+
+    X_new4 = np.array([[0.0, 1.0, 0.0]], dtype=np.float32)
+    onnx_result1 = ort_session.run([output_name], {input_name: X_new4})[0]
+    print(f"onnx模型预测概率[0.0, 1.0, 0.0]:{onnx_result1[0][0]:.6f}")
 
 
